4da2e315f4e03e0ada4dcb35ed02bef3094e6aa1
LightIce10 <145585156+LightIce10@users.noreply.github.com>
2024-04-10 09:57:06 -0600

    Update script.js

--- script.js
+++ script.js
@@ -73,8 +73,18 @@ function changeSize() {
 
 function blackBTN() {
   document.getElementById("display").style.background = "black";
+  
+  document.getElementById("display").style.background = ;
+  document.getElementById("rgb-line").innerHTML = "rgb(0, 0, 0)";
+  rVal.value = 0
+  gVal.value = 0
+  bVal.value = 0
 }
 
 function whiteBTN() {
   document.getElementById("display").style.background = "white";
+  document.getElementById("rgb-line").innerHTML = "rgb(255, 255, 255)";
+  rVal.value = 255
+  gVal.value = 255
+  bVal.value = 255
 }
